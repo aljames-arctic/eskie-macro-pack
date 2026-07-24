ce090a17aa386c64f1687a96a4920f974af1068f
aljames-arctic <bakanabaka.vtt@gmail.com>
2026-07-24 03:01:10 +0000

    refactor: rename leading-underscore index files to index.js

---
+++ src/animation/index.js
@@ -0,0 +1,15 @@
+import { effect } from './effects/index.js';
+import { mask } from './mask/index.js';
+import { overlay } from './scene-overlays/index.js';
+import { showcase } from './showcase/index.js';
+import { traps } from './traps/index.js';
+import { utils } from './utils/index.js';
+
+export const animation = {
+    effect,
+    mask,
+    overlay,
+    showcase,
+    traps,
+    utils,
+};

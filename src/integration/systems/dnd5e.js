844b192d22659549fa9bfafc32d191e07bf2405d
aljames-arctic <bakanabaka.vtt@gmail.com>
2026-08-28 21:25:46 +0000

    refactor(system): completely absorb dnd5e.js into Dnd5eSystemAdapter and remove redundant integration/systems directory

--- src/integration/systems/dnd5e.js
+++
@@ -1,9 +0,0 @@
-import { adapter } from '../../adapters/index.js';
-
-function getSpellLevel(config = {}) {
-    return adapter.getSpellLevel(config);
-}
-
-export const dnd5e = {
-    getSpellLevel,
-};
